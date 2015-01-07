9ef890d03a024d6a55b3ed9c91b31abf91ca9c75
Jonas Wielicki <j.wielicki@sotecware.net>
2015-01-07 15:36:59 +0100

    Add callback node hooks

--- asyncio_xmpp/hooks.py
+++ asyncio_xmpp/hooks.py
@@ -7,21 +7,28 @@ class NodeHooks:
         self._map = dict()
 
     def _setdefault(self, key):
-        return self._map.setdefault(key, (set(), set()))
+        return self._map.setdefault(key, (set(), set(), list()))
 
     def __contains__(self, key):
         try:
-            queues, futures = self._map[key]
+            queues, futures, cbs = self._map[key]
         except KeyError:
             return False
-        return queues or futures
+        return queues or futures or cbs
 
     def add_future(self, key, future):
         """
         Add a one-shot *future* listener for the given *key*.
         """
-        _, futures = self._setdefault(key)
+        _, futures, _ = self._setdefault(key)
         futures.add(future)
+
+    def add_callback(self, key, fn):
+        """
+        Add a continous callback *fn* listener for the given *key*.
+        """
+        _, _, cbs = self._setdefault(key)
+        cbs.append(fn)
 
     def add_queue(self, key, queue):
         """
@@ -31,7 +38,7 @@ class NodeHooks:
         queue, the element will be dropped (and no error will be raised from
         :meth:`broadcast`).
         """
-        queues, _ = self._setdefault(key)
+        queues, _, _ = self._setdefault(key)
         queues.add(queue)
 
     def broadcast_error(self, exc):
@@ -40,26 +47,27 @@ class NodeHooks:
         *exc* is set as exception. For queues, nothing is done (there is no
         out-of-band mechanism for posting exceptions).
 
-        Queues remain listening to their respective keys (use :meth:`close` to
-        broadcast errors and remove queues).
+        Queues and callbacks remain listening to their respective keys (use
+        :meth:`close` to broadcast errors and remove queues).
         """
         to_remove = set()
 
-        for key, (queues, futures) in self._map.items():
+        for key, (queues, futures, cbs) in self._map.items():
             for future in futures:
                 future.set_exception(exc)
             futures.clear()
-            if not queues:
+            if not queues and not cbs:
                 to_remove.add(key)
 
         for key in to_remove:
             del self._map[key]
 
-    def _close_key(self, queues, futures, exc):
+    def _close_key(self, queues, futures, cbs, exc):
         for future in futures:
             future.set_exception(exc)
         futures.clear()
         queues.clear()
+        cbs.clear()
 
     def close(self, key, exc):
         """
@@ -68,20 +76,27 @@ class NodeHooks:
         signalling mechanism for queues).
         """
         try:
-            queues, futures = self._map[key]
+            queues, futures, cbs = self._map[key]
         except KeyError:
             return
         del self._map[key]
 
-        self._close_key(queues, futures, exc)
+        self._close_key(queues, futures, cbs, exc)
 
     def close_all(self, exc):
         """
         Close all listeners. The same rules as for :meth:`close` apply.
         """
-        for queues, futures in self._map.values():
-            self._close_key(queues, futures, exc)
+        for queues, futures, cbs in self._map.values():
+            self._close_key(queues, futures, cbs, exc)
         self._map.clear()
+
+    def remove_callback(self, key, fn):
+        _, _, cbs = self._map[key]
+        try:
+            cbs.remove(fn)
+        except ValueError:
+            raise KeyError(key) from None
 
     def remove_future(self, key, future):
         """
@@ -90,7 +105,7 @@ class NodeHooks:
         Raise :class:`KeyError`, if *key* does not exist or *future* was not
         registered for listening to *key*.
         """
-        _, futures = self._map[key]
+        _, futures, _ = self._map[key]
         try:
             futures.remove(future)
         except ValueError:
@@ -103,7 +118,7 @@ class NodeHooks:
         Raise :class:`KeyError`, if *key* does not exist or *future* was not
         registered for listening to *key*.
         """
-        queues, _ = self._map[key]
+        queues, _, _ = self._map[key]
         try:
             queues.remove(queue)
         except ValueError:
@@ -117,8 +132,8 @@ class NodeHooks:
         Return :data:`True` if *value* could be submitted to all targets,
         :data:`False` otherwise (e.g. if a queue ran full).
         """
-        queues, futures = self._map[key]
-        if not queues and not futures:
+        queues, futures, cbs = self._map[key]
+        if not queues and not futures and not cbs:
             del self._map[key]
             # raise KeyError :)
             self._map[key]
@@ -131,6 +146,9 @@ class NodeHooks:
                 queue.put_nowait(value)
             except asyncio.QueueFull:
                 some_failed = True
+
+        for cb in cbs:
+            cb(value)
 
         for future in copied_futures:
             future.set_result(value)
