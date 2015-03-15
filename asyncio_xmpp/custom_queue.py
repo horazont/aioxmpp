adf9178171dcee7af8cda687c70e00bdf142a50c
Jonas Wielicki <j.wielicki@sotecware.net>
2015-03-15 15:09:50 +0100

    Rewrite custom queue

--- asyncio_xmpp/custom_queue.py
+++ asyncio_xmpp/custom_queue.py
@@ -1,78 +1,56 @@
 import asyncio
 import collections
 
-class AsyncDeque:
-    def __init__(self, initial_data=[], *, loop=None):
-        super().__init__()
-        self._data = collections.deque(initial_data)
-        self._loop = loop
-        self._non_empty = asyncio.Event()
-        self._non_empty.clear()
 
-    def __contains__(self, item):
-        return item in self._data
+class AsyncDeque:
+    def __init__(self, *, loop=None):
+        super().__init__()
+        self._loop = loop
+        self._data = collections.deque()
+        self._non_empty = asyncio.Event(loop=self._loop)
+        self._non_empty.clear()
 
     def __len__(self):
         return len(self._data)
 
-    def put_nowait(self, value):
-        self._data.append(value)
-        if self._data:
-            self._non_empty.set()
-
-    def putleft_nowait(self, value):
-        self._data.appendleft(value)
-        if self._data:
-            self._non_empty.set()
-
-    def clear(self):
-        self._data.clear()
-        self._non_empty.clear()
-
-    def extend(self, value):
-        self._data.extend(value)
-        if self._data:
-            self._non_empty.set()
-
-    def extendleft(self, value):
-        self._data.extend(value)
-        if self._data:
-            self._non_empty.set()
+    def __contains__(self, obj):
+        return obj in self._data
 
     def empty(self):
-        return bool(self._data)
+        return not self._non_empty.is_set()
 
-    @asyncio.coroutine
-    def getright(self):
-        while not self._data:
-            yield from self._non_empty.wait()
-        result = self._data.pop()
+    def put_nowait(self, obj):
+        self._data.append(obj)
+        self._non_empty.set()
+
+    def putleft_nowait(self, obj):
+        self._data.appendleft(obj)
+        self._non_empty.set()
+
+    def get_nowait(self):
+        try:
+            item = self._data.popleft()
+        except IndexError:
+            raise asyncio.QueueEmpty() from None
         if not self._data:
             self._non_empty.clear()
-        return result
+        return item
+
+    def getright_nowait(self):
+        try:
+            item = self._data.pop()
+        except IndexError:
+            raise asyncio.QueueEmpty() from None
+        if not self._data:
+            self._non_empty.clear()
+        return item
 
     @asyncio.coroutine
     def get(self):
         while not self._data:
             yield from self._non_empty.wait()
-        result = self._data.popleft()
-        if not self._data:
-            self._non_empty.clear()
-        return result
+        return self.get_nowait()
 
-    def get_nowait(self):
-        try:
-            return self._data.popleft()
-        except IndexError:
-            raise asyncio.QueueEmpty()
-
-    def getright_nowait(self):
-        try:
-            return self._data.pop()
-        except IndexError:
-            raise asyncio.QueueEmpty()
-
-    def remove(self, item):
-        self._data.remove(item)
-        if not self._data:
-            self._non_empty.clear()
+    def clear(self):
+        self._data.clear()
+        self._non_empty.clear()
---
+++ tests/test_custom_queue.py
@@ -0,0 +1,159 @@
+import asyncio
+import unittest
+
+import asyncio_xmpp.custom_queue as custom_queue
+
+from .testutils import run_coroutine
+
+
+class TestAsyncDeque(unittest.TestCase):
+    def setUp(self):
+        self.loop = asyncio.get_event_loop()
+        self.q = custom_queue.AsyncDeque(loop=self.loop)
+
+    def test_put_get_cycle_nowait(self):
+        self.q.put_nowait(1)
+        self.q.put_nowait(2)
+        self.q.put_nowait(3)
+
+        self.assertEqual(
+            1,
+            self.q.get_nowait()
+        )
+        self.assertEqual(
+            2,
+            self.q.get_nowait()
+        )
+        self.assertEqual(
+            3,
+            self.q.get_nowait()
+        )
+
+    def test_putleft_getright_cycle_nowait(self):
+        self.q.putleft_nowait(1)
+        self.q.putleft_nowait(2)
+        self.q.putleft_nowait(3)
+
+        self.assertEqual(
+            1,
+            self.q.getright_nowait()
+        )
+        self.assertEqual(
+            2,
+            self.q.getright_nowait()
+        )
+        self.assertEqual(
+            3,
+            self.q.getright_nowait()
+        )
+
+    def test_len(self):
+        self.assertEqual(0, len(self.q))
+        self.q.put_nowait(1)
+        self.assertEqual(1, len(self.q))
+        self.q.put_nowait(1)
+        self.assertEqual(2, len(self.q))
+        self.q.get_nowait()
+        self.assertEqual(1, len(self.q))
+
+    def test_contains(self):
+        self.assertNotIn(1, self.q)
+        self.q.put_nowait(1)
+        self.assertIn(1, self.q)
+
+    def test_get(self):
+        @asyncio.coroutine
+        def putter():
+            yield from asyncio.sleep(0.001)
+            self.q.put_nowait(1)
+
+        _, v = run_coroutine(asyncio.gather(
+            putter(),
+            self.q.get()
+        ))
+
+        self.assertEqual(1, v)
+
+    def test_one_producer_many_consumers(self):
+        @asyncio.coroutine
+        def putter():
+            for i in range(20):
+                self.q.put_nowait(i)
+                yield from asyncio.sleep(0)
+
+        @asyncio.coroutine
+        def getter():
+            result = []
+            for i in range(4):
+                result.append((yield from self.q.get()))
+                yield from asyncio.sleep(0)
+            return result
+
+        _, vs1, vs2, vs3, vs4, vs5 = run_coroutine(asyncio.gather(
+            putter(),
+            getter(),
+            getter(),
+            getter(),
+            getter(),
+            getter(),
+        ))
+
+        self.assertSequenceEqual(
+            range(20),
+            sorted(vs1+vs2+vs3+vs4+vs5)
+        )
+
+    def test_one_consumer_many_producers(self):
+        @asyncio.coroutine
+        def putter(n0):
+            for i in range(n0, n0+4):
+                self.q.put_nowait(i)
+                yield from asyncio.sleep(0)
+
+        @asyncio.coroutine
+        def getter():
+            result = []
+            for i in range(20):
+                result.append((yield from self.q.get()))
+                yield from asyncio.sleep(0)
+            return result
+
+        _, _, _, _, _, vs = run_coroutine(asyncio.gather(
+            putter(0),
+            putter(4),
+            putter(8),
+            putter(12),
+            putter(16),
+            getter()
+        ))
+
+        self.assertSequenceEqual(
+            range(20),
+            sorted(vs)
+        )
+
+    def test_empty(self):
+        self.assertTrue(self.q.empty())
+        self.q.put_nowait(1)
+        self.assertFalse(self.q.empty())
+        self.q.get_nowait()
+        self.assertTrue(self.q.empty())
+
+    def test_raise_asyncio_QueueEmpty_on_empty_get(self):
+        with self.assertRaises(asyncio.QueueEmpty):
+            self.q.get_nowait()
+
+        with self.assertRaises(asyncio.QueueEmpty):
+            self.q.getright_nowait()
+
+    def test_clear(self):
+        self.q.put_nowait(1)
+        self.q.put_nowait(2)
+        self.q.clear()
+        self.assertTrue(self.q.empty())
+        with self.assertRaises(asyncio.QueueEmpty):
+            self.q.get_nowait()
+
+    def tearDown(self):
+        del self.q
+        del self.loop
