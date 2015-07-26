eb73ce95e4b005cb8a61ef74bf2271721a9d8e3b
Jonas Wielicki <j.wielicki@sotecware.net>
2015-07-26 17:35:57 +0200

    Add module for state machines

---
+++ aioxmpp/statemachine.py
@@ -0,0 +1,157 @@
+"""
+:mod:`statemachine` -- Utils for implementing a state machine
+#############################################################
+"""
+import asyncio
+
+
+class OrderedStateSkipped(ValueError):
+    """
+    This exception signals that a state has been skipped in a
+    :class:`OrderedStateMachine` and cannot be waited for anymore.
+
+    .. attribute:: skipped_state
+
+       The state which a routine attempted to wait for and which cannot be
+       reached by the state machine anymore.
+
+    """
+
+    def __init__(self, skipped_state):
+        super().__init__("state {} has been skipped".format(skipped_state))
+        self.skipped_state = skipped_state
+
+
+class OrderedStateMachine:
+    """
+    :class:`OrderedStateMachine` provides facilities to implement a state
+    machine. Besides storing the state, it provides coroutines which allow to
+    wait for a specific state.
+
+    The state machine uses `initial_state` as initial state. States used by
+    :class:`OrderedStateMachine` must be ordered; a sanity check is performed
+    by checking if the `initial_state` is less than itself. If that check
+    fails, :class:`TypeError` is raised.
+
+    Reading and manipulating the state:
+
+    .. autoattribute:: state
+
+    .. automethod:: rewind
+
+    Waiting for specific states:
+
+    .. automethod:: wait_for
+
+    .. automethod:: wait_for_at_least
+    """
+    def __init__(self, initial_state, *, loop=None):
+        try:
+            initial_state < initial_state
+        except (TypeError, AttributeError):
+            raise TypeError("states must be ordered")
+
+        self._state = initial_state
+        self._least_waiters = []
+        self._exact_waiters = []
+        self.loop = loop if loop is not None else asyncio.get_event_loop()
+
+    @property
+    def state(self):
+        """
+        The current state of the state machine. Writing to this attribute
+        advances the state of the state machine.
+
+        Attempting to change the state to a state which is *less* than the
+        current state will result in a :class:`ValueError` exception; an
+        :class:`OrderedStateMachine` can only move forwards.
+
+        Any coroutines waiting for a specific state to be reached will be woken
+        up appropriately, see the specific methods for details.
+        """
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        if new_state < self._state:
+            raise ValueError("cannot rewind OrderedStateMachine "
+                             "({} < {})".format(
+                                 new_state, self._state))
+        self._state = new_state
+
+        new_waiters = []
+        for least_state, fut in self._least_waiters:
+            if fut.done():
+                continue
+            if new_state >= least_state:
+                fut.set_result(None)
+                continue
+            new_waiters.append((least_state, fut))
+        self._least_waiters[:] = new_waiters
+
+        new_waiters = []
+        for expected_state, fut in self._exact_waiters:
+            if fut.done():
+                continue
+            if expected_state == new_state:
+                fut.set_result(None)
+                continue
+            if expected_state < new_state:
+                fut.set_exception(OrderedStateSkipped(expected_state))
+                continue
+            new_waiters.append((expected_state, fut))
+        self._exact_waiters[:] = new_waiters
+
+    def rewind(self, new_state):
+        """
+        Rewind can be used as an exceptional way to roll back the state of a
+        :class:`OrderedStateMachine`.
+
+        Rewinding is not the usual use case for an
+        :class:`OrderedStateMachine`. Usually, if the current state `A` is
+        greater than any given state `B`, it is assumed that state `B` cannot
+        be reached anymore (which makes :meth:`wait_for` raise).
+
+        It may make sense to go backwards though, and in cases where the
+        ability to go backwards is sensible even if routines which previously
+        attempted to wait for the state you are going backwards to failed,
+        using a :class:`OrderedStateMachine` is still a good idea.
+        """
+        if new_state > self._state:
+            raise ValueError("cannot forward using rewind "
+                             "({} > {})".format(new_state, self._state))
+        self._state = new_state
+
+    @asyncio.coroutine
+    def wait_for(self, new_state):
+        """
+        Wait for an exact state `new_state` to be reached by the state
+        machine.
+
+        If the state is skipped, that is, if a state which is greater than
+        `new_state` is written to :attr:`state`, the coroutine raises
+        :class:`OrderedStateSkipped` exception as it is not possible anymore
+        that it can return successfully (see :attr:`state`).
+        """
+        if self._state == new_state:
+            return
+
+        if self._state > new_state:
+            raise OrderedStateSkipped(new_state)
+
+        fut = asyncio.Future(loop=self.loop)
+        self._exact_waiters.append((new_state, fut))
+        yield from fut
+
+    @asyncio.coroutine
+    def wait_for_at_least(self, new_state):
+        """
+        Wait for a state to be entered which is greater than or equal to
+        `new_state` and return.
+        """
+        if self._state >= new_state:
+            return
+
+        fut = asyncio.Future(loop=self.loop)
+        self._least_waiters.append((new_state, fut))
+        yield from fut
---
+++ tests/test_statemachine.py
@@ -0,0 +1,197 @@
+import asyncio
+import functools
+import unittest
+
+from enum import Enum
+
+import aioxmpp.statemachine as statemachine
+
+from aioxmpp.testutils import run_coroutine
+
+
+@functools.total_ordering
+class States(Enum):
+    STATE1 = 1
+    STATE2 = 2
+    STATE3 = 3
+    STATE4 = 4
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class TestOrderedStateSkipped(unittest.TestCase):
+    def test_is_value_error(self):
+        self.assertTrue(issubclass(
+            statemachine.OrderedStateSkipped,
+            ValueError
+        ))
+
+    def test_init(self):
+        exc = statemachine.OrderedStateSkipped(States.STATE1)
+        self.assertRegex(
+            str(exc),
+            r"state [^ ]+ has been skipped"
+        )
+        self.assertIs(exc.skipped_state, States.STATE1)
+
+
+class TestOrderedStateMachine(unittest.TestCase):
+    def setUp(self):
+        self.loop = asyncio.get_event_loop()
+        self.osm = statemachine.OrderedStateMachine(
+            States.STATE1,
+            loop=self.loop)
+
+    def test_init(self):
+        osm = statemachine.OrderedStateMachine(
+            States.STATE1)
+        self.assertEqual(
+            States.STATE1,
+            osm.state
+        )
+
+    def test_init_rejects_unordered_state_type(self):
+        class OtherStates(Enum):
+            FOO = 1
+            BAR = 2
+
+        with self.assertRaisesRegex(TypeError,
+                                    "states must be ordered"):
+            osm = statemachine.OrderedStateMachine(OtherStates.FOO)
+
+    def test_wait_for_at_least(self):
+        state_tasks = {
+            state: asyncio.async(self.osm.wait_for_at_least(state),
+                                 loop=self.loop)
+            for state in States
+        }
+
+        run_coroutine(asyncio.sleep(0))
+
+        self.osm.state = States.STATE3
+
+        run_coroutine(asyncio.sleep(0.01))
+
+        for state, task in state_tasks.items():
+            if state <= States.STATE3:
+                self.assertTrue(task.done(), state)
+                self.assertIsNone(task.result())
+            else:
+                self.assertFalse(task.done(), state)
+
+    def test_wait_for_at_least_checks_current_state(self):
+        state_tasks = {
+            state: asyncio.async(self.osm.wait_for_at_least(state),
+                                 loop=self.loop)
+            for state in States
+        }
+
+        self.osm.state = States.STATE3
+
+        run_coroutine(asyncio.sleep(0))
+
+        for state, task in state_tasks.items():
+            if state <= States.STATE3:
+                self.assertTrue(task.done(), state)
+                self.assertIsNone(task.result())
+            else:
+                self.assertFalse(task.done(), state)
+
+    def test_wait_for_at_least_can_be_cancelled(self):
+        state_tasks = {
+            state: asyncio.async(self.osm.wait_for_at_least(state),
+                                 loop=self.loop)
+            for state in States
+        }
+
+        run_coroutine(asyncio.sleep(0))
+
+        for task in state_tasks.values():
+            task.cancel()
+
+        run_coroutine(asyncio.sleep(0))
+
+        self.osm.state = States.STATE3
+
+        run_coroutine(asyncio.sleep(0))
+
+    def test_state_rejects_rewinding(self):
+        self.osm.state = States.STATE2
+        with self.assertRaisesRegex(
+                ValueError,
+                "cannot rewind OrderedStateMachine"):
+            self.osm.state = States.STATE1
+        self.osm.state = States.STATE2
+
+    def test_wait_for(self):
+        state_tasks = {
+            state: asyncio.async(self.osm.wait_for(state),
+                                 loop=self.loop)
+            for state in States
+        }
+
+        run_coroutine(asyncio.sleep(0))
+
+        self.osm.state = States.STATE3
+
+        run_coroutine(asyncio.sleep(0.01))
+
+        for state, task in state_tasks.items():
+            # STATE1 triggers immediately, it is the current state when the
+            # coroutine is called
+            if state in [States.STATE3, States.STATE1]:
+                self.assertTrue(task.done(), state)
+                self.assertIsNone(task.result())
+            elif state < States.STATE3:
+                self.assertTrue(task.done(), state)
+                self.assertIsInstance(
+                    task.exception(),
+                    statemachine.OrderedStateSkipped,
+                    state
+                )
+            else:
+                self.assertFalse(task.done(), state)
+
+    def test_wait_for_checks_immediately(self):
+        state_tasks = {
+            state: asyncio.async(self.osm.wait_for(state),
+                                 loop=self.loop)
+            for state in States
+        }
+
+        self.osm.state = States.STATE3
+
+        run_coroutine(asyncio.sleep(0.01))
+
+        for state, task in state_tasks.items():
+            if state == States.STATE3:
+                self.assertTrue(task.done(), state)
+                self.assertIsNone(task.result())
+            elif state < States.STATE3:
+                self.assertTrue(task.done(), state)
+                self.assertIsInstance(
+                    task.exception(),
+                    statemachine.OrderedStateSkipped,
+                    state
+                )
+            else:
+                self.assertFalse(task.done(), state)
+
+    def test_rewind_allows_going_back(self):
+        self.osm.state = States.STATE3
+        self.osm.rewind(States.STATE2)
+        self.assertEqual(
+            States.STATE2,
+            self.osm.state
+        )
+
+    def test_rewind_rejects_going_forward(self):
+        self.osm.state = States.STATE3
+        with self.assertRaisesRegex(ValueError,
+                                    "cannot forward using rewind"):
+            self.osm.rewind(States.STATE4)
+
+    def tearDown(self):
+        del self.osm
+        del self.loop
