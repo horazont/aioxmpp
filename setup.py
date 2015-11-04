cfd962b229ec1afb9984251dfd7f6a64dfd65420
Jonas Wielicki <j.wielicki@sotecware.net>
2015-11-04 17:06:44 +0100

    Source the version in the setup.py from the module

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
+import aioxmpp
+
 setup(
     name="aioxmpp",
-    version="0.4",
+    version=aioxmpp.__version__,
     description="Pure-python XMPP library for asyncio",
     long_description=long_description,
     url="https://github.com/horazont/aioxmpp",
