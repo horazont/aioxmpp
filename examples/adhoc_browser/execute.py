3357f771c54a88cdf81b1595d45c7bbbe8f462ea
Jonas Wielicki <j.wielicki@sotecware.net>
2017-02-06 18:59:23 +0100

    Add adhoc browser example

---
+++ examples/adhoc_browser/execute.py
@@ -0,0 +1,246 @@
+import asyncio
+import html
+
+import PyQt5.Qt as Qt
+
+import aioxmpp.adhoc
+
+from .utils import asyncify, asyncify_blocking
+from .ui.form import Ui_FormDialog
+
+
+class Executor(Qt.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_FormDialog()
+        self.ui.setupUi(self)
+        self.base_title = self.windowTitle()
+        self.session = None
+
+        self.action_buttons = {
+            aioxmpp.adhoc.ActionType.NEXT: self.ui.btn_next,
+            aioxmpp.adhoc.ActionType.PREV: self.ui.btn_prev,
+            aioxmpp.adhoc.ActionType.CANCEL: self.ui.btn_cancel,
+            aioxmpp.adhoc.ActionType.COMPLETE: self.ui.btn_complete,
+        }
+
+        self.ui.btn_close.clicked.connect(self._close)
+        self.ui.btn_next.clicked.connect(self._next)
+        self.ui.btn_prev.clicked.connect(self._prev)
+        self.ui.btn_cancel.clicked.connect(self._cancel)
+        self.ui.btn_complete.clicked.connect(self._complete)
+
+        self.fieldmap = {}
+        self.form_area = None
+
+    def _close(self):
+        self.close()
+
+    def _next(self):
+        self._action(aioxmpp.adhoc.ActionType.NEXT)
+
+    def _prev(self):
+        self._action(aioxmpp.adhoc.ActionType.PREV)
+
+    def _cancel(self):
+        self.close()
+
+    def _complete(self):
+        self._action(aioxmpp.adhoc.ActionType.COMPLETE)
+
+    @asyncify_blocking
+    @asyncio.coroutine
+    def run_with_session_fut(self, name, session_fut):
+        self.setWindowTitle("{} - {}".format(
+            name,
+            self.base_title
+        ))
+        self.show()
+        try:
+            self.session = yield from session_fut
+        except Exception as exc:
+            self.fail(exc)
+            return
+        self.response_received()
+
+    def _update_buttons(self):
+        status = self.session.status
+        if status != aioxmpp.adhoc.CommandStatus.EXECUTING:
+            self.ui.btn_cancel.hide()
+            self.ui.btn_next.hide()
+            self.ui.btn_prev.hide()
+            self.ui.btn_complete.hide()
+            self.ui.btn_close.show()
+        else:
+            self.ui.btn_close.hide()
+            allowed_actions = self.session.allowed_actions
+            for action, btn in self.action_buttons.items():
+                if action in allowed_actions:
+                    btn.show()
+                else:
+                    btn.hide()
+
+    def _update_notes(self):
+        notes = self.session.response.notes
+        if not notes:
+            self.ui.notes_area.hide()
+            return
+
+        self.ui.notes_area.show()
+        source_parts = []
+        for note in notes:
+            print(note.type_, note.body)
+            source_parts.append("<p><b>{}: </b>{}</p>".format(
+                html.escape(note.type_.value),
+                "</p><p>".join(html.escape(note.body or "").split("\n"))
+            ))
+
+        self.ui.notes_area.setText("\n".join(source_parts))
+
+    def _update_form(self):
+        payload = self.session.first_payload
+        if not isinstance(payload, aioxmpp.forms.Data):
+            self.ui.form_widget.hide()
+            return
+
+        self.ui.form_widget.show()
+        if payload.title:
+            self.ui.title.show()
+            self.ui.title.setText(payload.title)
+        else:
+            self.ui.title.hide()
+
+        if payload.instructions:
+            self.ui.instructions.show()
+            self.ui.instructions.setText(
+                "<p>{}</p>".format(
+                    "</p><p>".join(map(html.escape, payload.instructions))
+                )
+            )
+        else:
+            self.ui.instructions.hide()
+
+        if self.form_area is not None:
+            self.ui.form_widget.layout().removeWidget(self.form_area)
+            self.ui.form_widget.children().remove(self.form_area)
+            self.form_area.deleteLater()
+
+        self.form_area = Qt.QScrollArea()
+        layout = Qt.QFormLayout()
+        self.form_area.setLayout(layout)
+        self.ui.form_widget.layout().addWidget(self.form_area)
+
+        self.fieldmap = {}
+        for field in payload.fields:
+            if field.var == "FORM_TYPE":
+                continue
+            label, widget = None, None
+            if field.type_ == aioxmpp.forms.FieldType.FIXED:
+                label = Qt.QLabel(field.values[0])
+                layout.addRow(
+                    label
+                )
+            elif field.type_ in {aioxmpp.forms.FieldType.LIST_SINGLE,
+                                 aioxmpp.forms.FieldType.LIST_MULTI}:
+                label = Qt.QLabel(field.label)
+                widget = Qt.QListWidget()
+                for opt_value, opt_label in sorted(field.options.items()):
+                    item = Qt.QListWidgetItem(opt_label, widget)
+                    item.setData(Qt.Qt.UserRole, opt_value)
+                    widget.addItem(item)
+
+                if field.type_.is_multivalued:
+                    widget.setSelectionMode(
+                        Qt.QAbstractItemView.MultiSelection
+                    )
+                else:
+                    widget.setSelectionMode(
+                        Qt.QAbstractItemView.SingleSelection
+                    )
+                layout.addRow(label, widget)
+
+            elif field.type_ in {aioxmpp.forms.FieldType.TEXT_SINGLE,
+                                 aioxmpp.forms.FieldType.JID_SINGLE}:
+                label = Qt.QLabel(field.label)
+                widget = Qt.QLineEdit()
+                if field.values:
+                    widget.setText(field.values[0])
+                layout.addRow(label, widget)
+
+            elif field.type_ in {aioxmpp.forms.FieldType.TEXT_MULTI,
+                                 aioxmpp.forms.FieldType.JID_MULTI}:
+                label = Qt.QLabel(field.label)
+                widget = Qt.QTextEdit()
+                widget.setText("\n".join(field.values))
+                widget.setAcceptRichText(False)
+                layout.addRow(label, widget)
+
+            else:
+                self.fail("unhandled field type: {}".format(field.type_))
+
+            self.fieldmap[field.var] = label, widget
+
+    def _fill_form(self):
+        for field in self.session.first_payload.fields:
+            try:
+                _, widget = self.fieldmap[field.var]
+            except KeyError:
+                continue
+            if widget is None:
+                continue
+
+            if field.type_ in {aioxmpp.forms.FieldType.LIST_SINGLE,
+                               aioxmpp.forms.FieldType.LIST_MULTI}:
+                # widget is list widget
+                selected = []
+                for index in widget.selectionModel().selectedIndexes():
+                    selected.append(
+                        widget.model().data(
+                            index,
+                            Qt.Qt.UserRole
+                        )
+                    )
+
+                field.values[:] = selected
+
+            elif field.type_ in {aioxmpp.forms.FieldType.JID_SINGLE,
+                                 aioxmpp.forms.FieldType.TEXT_SINGLE}:
+                # widget is line edit
+                field.values[:] = [widget.text()]
+
+            elif field.type_ in {aioxmpp.forms.FieldType.JID_MULTI,
+                                 aioxmpp.forms.FieldType.TEXT_MULTI}:
+                field.values[:] = widget.text().split("\n")
+
+    def _action(self, type_):
+        self._fill_form()
+        self._submit_action(type_)
+
+    @asyncify_blocking
+    @asyncio.coroutine
+    def _submit_action(self, type_):
+        yield from self.session.proceed(action=type_)
+        self.response_received()
+
+    def response_received(self):
+        try:
+            self.ui.status.setText(str(self.session.status))
+            self._update_buttons()
+            self._update_notes()
+            self._update_form()
+        except Exception as exc:
+            self.fail(str(exc))
+
+    def fail(self, message):
+        Qt.QMessageBox.critical(
+            self.parent(),
+            "Error",
+            message,
+        )
+        self.close()
+
+    @asyncify
+    def closeEvent(self, ev):
+        if self.session is not None:
+            yield from self.session.close()
+        return super().closeEvent(ev)
---
+++ examples/adhoc_browser/main.py
@@ -0,0 +1,213 @@
+import asyncio
+import sys
+
+import PyQt5.Qt as Qt
+
+import aioxmpp
+
+from framework import Example
+
+from .utils import asyncify_blocking
+try:
+    from .ui.main import Ui_MainWindow
+except ImportError:
+    print("You didn’t run make, I’ll try to do it for you...",
+          file=sys.stderr)
+    import subprocess
+    try:
+        subprocess.check_call(["make"])
+        from .ui.main import Ui_MainWindow
+    except Exception:
+        print("Nope, that didn’t work out. "
+              "You’ll have to fix that yourself. Sorry.",
+              file=sys.stderr)
+        raise
+
+from .execute import Executor
+
+if not hasattr(asyncio, "async"):
+    setattr(asyncio, "async", asyncio.ensure_future)
+
+
+class DiscoItemsModel(Qt.QAbstractTableModel):
+    COLUMN_NAME = 0
+    COLUMN_JID = 1
+    COLUMN_NODE = 2
+    COLUMN_COUNT = 3
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._items = []
+
+    def replace(self, items):
+        self.beginResetModel()
+        self._items[:] = items
+        self.endResetModel()
+
+    def rowCount(self, parent):
+        if parent.isValid():
+            return 0
+        return len(self._items)
+
+    def columnCount(self, parent):
+        return self.COLUMN_COUNT
+
+    def data(self, index, role):
+        if role != Qt.Qt.DisplayRole:
+            return
+        if not index.isValid():
+            return
+
+        item = self._items[index.row()]
+        return {
+            self.COLUMN_NAME: item.name,
+            self.COLUMN_JID: str(item.jid),
+            self.COLUMN_NODE: item.node or "",
+        }.get(index.column())
+
+    def headerData(self, section, orientation, role):
+        if orientation != Qt.Qt.Horizontal:
+            return
+        if role != Qt.Qt.DisplayRole:
+            return
+        return {
+            self.COLUMN_NAME: "Name",
+            self.COLUMN_JID: "JID",
+            self.COLUMN_NODE: "Node",
+        }.get(section)
+
+
+class MainWindow(Qt.QMainWindow):
+    def __init__(self, close_fut):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_scan.clicked.connect(
+            self.scan
+        )
+
+        self.disco_model = DiscoItemsModel()
+        self.ui.disco_items.setModel(self.disco_model)
+        self.ui.disco_items.activated.connect(self.disco_item_activated)
+
+        self.commands_model = DiscoItemsModel()
+        self.ui.commands.setModel(self.commands_model)
+        self.ui.commands.activated.connect(self.command_activated)
+
+        self._close_fut = close_fut
+
+    def disco_item_activated(self, index):
+        if not index.isValid():
+            return
+
+        jid = self.disco_model.data(
+            self.disco_model.index(
+                index.row(),
+                self.disco_model.COLUMN_JID,
+                index.parent(),
+            ),
+            Qt.Qt.DisplayRole)
+
+        self.ui.target_jid.setText(jid)
+        self.scan()
+
+    @asyncify_blocking
+    @asyncio.coroutine
+    def command_activated(self, index):
+        if not index.isValid():
+            return
+
+        jid = aioxmpp.JID.fromstr(
+            self.commands_model.data(
+                self.commands_model.index(
+                    index.row(),
+                    self.disco_model.COLUMN_JID,
+                    index.parent(),
+                ),
+                Qt.Qt.DisplayRole)
+        )
+
+        node = self.commands_model.data(
+            self.commands_model.index(
+                index.row(),
+                self.disco_model.COLUMN_NODE,
+                index.parent(),
+            ),
+            Qt.Qt.DisplayRole)
+
+        session_fut = asyncio.ensure_future(
+            self.adhoc_svc.execute(jid, node)
+        )
+        dlg = Executor(self)
+        dlg.run_with_session_fut(node, session_fut)
+
+    @asyncify_blocking
+    @asyncio.coroutine
+    def scan(self, *args, **kwargs):
+        jid = aioxmpp.JID.fromstr(self.ui.target_jid.text())
+        items = yield from self.disco_svc.query_items(
+            jid
+        )
+        commands = yield from self.adhoc_svc.get_commands(
+            jid
+        )
+
+        self.disco_model.replace(items.items)
+        self.commands_model.replace(commands)
+
+    def closeEvent(self, ev):
+        if self._close_fut.done():
+            return super().closeEvent(ev)
+        self._close_fut.set_result(None)
+
+
+class AdHocBrowser(Example):
+    def __init__(self):
+        super().__init__()
+        self._close = asyncio.Future()
+        self._mainwindow = MainWindow(self._close)
+
+    @asyncio.coroutine
+    def _main(self):
+        self._mainwindow.client = self.client
+        self._mainwindow.disco_svc = self.disco_svc
+        self._mainwindow.adhoc_svc = self.adhoc_svc
+        self._mainwindow.show()
+        try:
+            yield from self._close
+        except:
+            self._mainwindow.close()
+
+    def _established(self):
+        self._mainwindow.statusBar().showMessage(
+            "connected as {}".format(self.client.local_jid)
+        )
+        if not self._mainwindow.ui.target_jid.text():
+            self._mainwindow.ui.target_jid.setText(
+                str(self.client.local_jid.domain)
+            )
+
+    @asyncio.coroutine
+    def run_example(self):
+        def kill(*args):
+            nonlocal task
+            if task is not None:
+                task.cancel()
+
+        self.client = self.make_simple_client()
+        self.disco_svc = self.client.summon(aioxmpp.DiscoClient)
+        self.adhoc_svc = self.client.summon(aioxmpp.AdHocClient)
+        self.client.on_failure.connect(kill)
+        self.client.on_stream_established.connect(self._established)
+
+        cm = self.client.connected()
+        aexit = cm.__aexit__
+        yield from cm.__aenter__()
+        try:
+            task = asyncio.ensure_future(self._main())
+            yield from task
+        except:
+            if not (yield from aexit(*sys.exc_info())):
+                raise
+        else:
+            yield from aexit(None, None, None)
