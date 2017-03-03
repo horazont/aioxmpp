6bbb8a303533d9193e4201adb2cfaaec832f10a0
Jonas Wielicki <j.wielicki@sotecware.net>
2017-03-03 15:07:11 +0100

    Fix missing license headers in adhoc_browser example and utils

--- utils/buildui.py
+++ utils/buildui.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+########################################################################
+# File name: buildui.py
+# This file is part of: aioxmpp
+#
+# LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+########################################################################
 
 # pyuic is not installed on my system, so here we go with a workaround
 
