ae876a0cdcfb094a3ac0055f2e69298fd3108a34
Sebastian Riese <s.riese@zombofant.net>
2017-03-25 14:58:51 +0100

    Implement the service for XEP-0084 User Avatar

--- aioxmpp/testutils.py
+++ aioxmpp/testutils.py
@@ -130,6 +130,7 @@ class ConnectedClientMock(unittest.mock.Mock):
         self.stream_features = nonza.StreamFeatures()
         self.stream.on_message_received = callbacks.AdHocSignal()
         self.stream.on_presence_received = callbacks.AdHocSignal()
+        self.stream.on_stream_destroyed = callbacks.AdHocSignal()
         self.stream.send_iq_and_wait_for_reply = CoroutineMock()
         self.stream.send = CoroutineMock()
         self.stream.enqueue_stanza = self.stream.enqueue
