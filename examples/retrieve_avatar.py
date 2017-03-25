1a805caec11bdf199c12c82165230c86c644d8c8
Sebastian Riese <s.riese@zombofant.net>
2017-03-25 17:09:26 +0100

    Add examples demonstrating the XEP-0084 User Avatar support
    * examples/retrieve_avatar.py retrieves a user avatar and stores
      it in a file
    * examples/set_avatar.py sets the user avatar (optionally
      wiping it)

---
+++ examples/retrieve_avatar.py
@@ -0,0 +1,91 @@
+########################################################################
+# File name: retrieve_avatar.py
+# This file is part of: aioxmpp
+#
+# LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+########################################################################
+import asyncio
+import argparse
+import configparser
+
+import aioxmpp
+import aioxmpp.avatar
+
+from framework import Example, exec_example
+
+class Avatar(Example):
+    def prepare_argparse(self):
+        super().prepare_argparse()
+
+        # this gives a nicer name in argparse errors
+        def jid(s):
+            return aioxmpp.JID.fromstr(s)
+
+        self.argparse.add_argument(
+            "output_file",
+            help="the file the retrieved avatar image will be written to."
+        )
+
+        self.argparse.add_argument(
+            "--remote-jid",
+            type=jid,
+            help="the jid of which to retrieve the avatar"
+        )
+
+    def configure(self):
+        super().configure()
+
+        self.output_file = self.args.output_file
+        self.remote_jid = self.args.remote_jid
+        if self.remote_jid is None:
+            try:
+                self.remote_jid = aioxmpp.JID.fromstr(
+                    self.config.get("avatar", "remote_jid")
+                )
+            except (configparser.NoSectionError,
+                    configparser.NoOptionError):
+                self.remote_jid = aioxmpp.JID.fromstr(
+                    input("Remote JID> ")
+                )
+
+    def make_simple_client(self):
+        client = super().make_simple_client()
+        self.avatar = client.summon(aioxmpp.avatar.AvatarClient)
+        return client
+
+    @asyncio.coroutine
+    def run_simple_example(self):
+        metadata = yield from self.avatar.get_avatar_metadata(
+            self.remote_jid
+        )
+
+        for metadatum in metadata["image/png"]:
+            if metadatum.image_in_pubsub:
+                image = yield from metadatum.get_image_bytes()
+                with open(self.output_file, "wb") as avatar_image:
+                    avatar_image.write(image)
+                return
+
+        print("retrieving avatar failed: no avatar in pubsub")
+
+    @asyncio.coroutine
+    def run_example(self):
+        yield from super().run_example()
+
+if __name__ == "__main__":
+    exec_example(Avatar())
---
+++ examples/set_avatar.py
@@ -0,0 +1,74 @@
+########################################################################
+# File name: set_avatar.py
+# This file is part of: aioxmpp
+#
+# LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+########################################################################
+import asyncio
+import argparse
+
+import aioxmpp
+import aioxmpp.avatar
+
+from framework import Example, exec_example
+
+class Avatar(Example):
+    def prepare_argparse(self):
+        super().prepare_argparse()
+
+        self.argparse.add_argument(
+            "avatar_file",
+            help="the file PNG image that will be used as avatar."
+        )
+
+        self.argparse.add_argument(
+            "--wipe-avatar",
+            action="store_true",
+            default=False,
+            help="set the avatar to no avatar when exiting."
+        )
+
+    def configure(self):
+        super().configure()
+        self.avatar_file = self.args.avatar_file
+        self.wipe_avatar = self.args.wipe_avatar
+
+    def make_simple_client(self):
+        client = super().make_simple_client()
+        self.avatar = client.summon(aioxmpp.avatar.AvatarServer)
+        return client
+
+    @asyncio.coroutine
+    def run_simple_example(self):
+        with open(self.avatar_file, "rb") as f:
+            image_data = f.read()
+
+        avatar_set = aioxmpp.avatar.AvatarSet()
+        avatar_set.add_avatar_image("image/png", image_bytes=image_data)
+
+        yield from self.avatar.publish_avatar_set(avatar_set)
+
+        if self.wipe_avatar:
+            yield from self.avatar.disable_avatar()
+
+    @asyncio.coroutine
+    def run_example(self):
+        yield from super().run_example()
+
+if __name__ == "__main__":
+    exec_example(Avatar())
