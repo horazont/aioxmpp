horazont/aioxmpp

commit e4388f72ff91bc8ee3eaa62bc4de9b60e2f1633d

Add stanza argument to disco.Node.iter_{items,features,identities}

aioxmpp/disco/service.py
@@ -90,8 +90,20 @@ class Node(object):
         self._identities = {}
         self._features = set()
 
-    def iter_identities(self):
+    def iter_identities(self, stanza):
         """
+        Return an iterator of tuples describing the identities of the node.
+
+        :param stanza: The IQ request stanza
+        :type stanza: :class:`~aioxmpp.IQ`
+        :rtype: iterable of (:class:`str`, :class:`str`, :class:`str` or :data:`None`, :class:`str` or :data:`None`) tuples
+        :return: :xep:`30` identities of this node
+
+        `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
+        used to hide a node depending on who is asking. If the returned
+        iterable is empty, the :class:`~.DiscoServer` returns an
+        ``<item-not-found/>`` error.
+
         Return an iterator which yields tuples consisting of the category, the
         type, the language code and the name of each identity declared in this
         :class:`Node`.
@@ -105,21 +117,39 @@ class Node(object):
             if not names:
                 yield category, type_, None, None
 
-    def iter_features(self):
+    def iter_features(self, stanza):
         """
-        Return an iterator which yields the `var` values of each feature
-        declared in this :class:`Node`, including the statically declared
-        :xep:`30` features.
+        Return an iterator which yields the features of the node.
+
+        :param stanza: The IQ request stanza
+        :type stanza: :class:`~aioxmpp.IQ`
+        :rtype: iterable of :class:`str`
+        :return: :xep:`30` features of this node
+
+        `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
+        used to filter the list according to who is asking (not recommended).
+
+        The features are returned as strings. The features demanded by
+        :xep:`30` are always returned.
+
         """
         return itertools.chain(
             iter(self.STATIC_FEATURES),
             iter(self._features)
         )
 
-    def iter_items(self):
+    def iter_items(self, stanza):
         """
-        Return an iterator which yields the :class:`.xso.Item` objects which
-        this node holds.
+        Return an iterator which yields the items of the node.
+
+        :param stanza: The IQ request stanza
+        :type stanza: :class:`~aioxmpp.IQ`
+        :rtype: iterable of :class:`~.disco.xso.Item`
+        :return: Items of the node
+
+        `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
+        used to localize the list to the language of the stanza or filter it
+        according to who is asking.
 
         A bare :class:`Node` cannot hold any items and will thus return an
         iterator which does not yield any element.
@@ -214,7 +244,7 @@ class StaticNode(Node):
         super().__init__()
         self.items = []
 
-    def iter_items(self):
+    def iter_items(self, stanza):
         return iter(self.items)
 
 
@@ -300,7 +330,7 @@ class DiscoServer(service.Service, Node):
 
         response = disco_xso.InfoQuery()
 
-        for category, type_, lang, name in node.iter_identities():
+        for category, type_, lang, name in node.iter_identities(iq):
             response.identities.append(disco_xso.Identity(
                 category=category,
                 type_=type_,
@@ -313,7 +343,7 @@ class DiscoServer(service.Service, Node):
                 condition=(namespaces.stanzas, "item-not-found"),
             )
 
-        response.features.update(node.iter_features())
+        response.features.update(node.iter_features(iq))
 
         return response
 
@@ -332,7 +362,7 @@ class DiscoServer(service.Service, Node):
             )
 
         response = disco_xso.ItemsQuery()
-        response.items.extend(node.iter_items())
+        response.items.extend(node.iter_items(iq.from_))
 
         return response
 

tests/disco/test_service.py
@@ -47,15 +47,15 @@ class TestNode(unittest.TestCase):
         n = disco_service.Node()
         self.assertSequenceEqual(
             [],
-            list(n.iter_identities())
+            list(n.iter_identities(unittest.mock.sentinel.stanza))
         )
         self.assertSetEqual(
             {namespaces.xep0030_info},
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
         self.assertSequenceEqual(
             [],
-            list(n.iter_items())
+            list(n.iter_items(unittest.mock.sentinel.stanza))
         )
 
     def test_register_feature_adds_the_feature(self):
@@ -70,7 +70,7 @@ class TestNode(unittest.TestCase):
                 "uri:foo",
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         cb.assert_called_with()
@@ -92,7 +92,7 @@ class TestNode(unittest.TestCase):
                 "uri:bar",
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         self.assertFalse(cb.mock_calls)
@@ -123,7 +123,7 @@ class TestNode(unittest.TestCase):
                 "uri:bar",
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         n.unregister_feature("uri:foo")
@@ -136,7 +136,7 @@ class TestNode(unittest.TestCase):
                 "uri:bar",
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         n.unregister_feature("uri:bar")
@@ -145,7 +145,7 @@ class TestNode(unittest.TestCase):
             {
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         cb.assert_called_with()
@@ -175,7 +175,7 @@ class TestNode(unittest.TestCase):
             {
                 namespaces.xep0030_info
             },
-            set(n.iter_features())
+            set(n.iter_features(unittest.mock.sentinel.stanza))
         )
 
         self.assertFalse(cb.mock_calls)
@@ -194,7 +194,7 @@ class TestNode(unittest.TestCase):
             {
                 ("client", "pc", None, None),
             },
-            set(n.iter_identities())
+            set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
         cb.assert_called_with()
@@ -222,7 +222,7 @@ class TestNode(unittest.TestCase):
             {
                 ("client", "pc", None, None),
             },
-            set(n.iter_identities())
+            set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
     def test_register_identity_with_names(self):
@@ -248,7 +248,7 @@ class TestNode(unittest.TestCase):
                 ("client", "pc",
                  structs.LanguageTag.fromstr("de"), "Testidentität"),
             },
-            set(n.iter_identities())
+            set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
     def test_unregister_identity_prohibits_removal_of_last_identity(self):
@@ -319,7 +319,7 @@ class TestNode(unittest.TestCase):
                  structs.LanguageTag.fromstr("de"), "Testidentität"),
                 ("foo", "bar", None, None),
             },
-            set(n.iter_identities())
+            set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
         cb = unittest.mock.Mock()
@@ -336,7 +336,7 @@ class TestNode(unittest.TestCase):
                 ("client", "pc",
                  structs.LanguageTag.fromstr("de"), "Testidentität"),
             },
-            set(n.iter_identities())
+            set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
 
@@ -358,7 +358,7 @@ class TestStaticNode(unittest.TestCase):
                 item1,
                 item2
             ],
-            list(self.n.iter_items())
+            list(self.n.iter_items(unittest.mock.sentinel.jid))
         )
 
 

commit 224474f89a6668ef0004070f2e0aa152195a3179

Default the arguments of iter_{identities,features,items} to None
This fixes the EntityCapsService which was broken since e4388f7.

aioxmpp/disco/service.py
@@ -90,19 +90,28 @@ class Node(object):
         self._identities = {}
         self._features = set()
 
-    def iter_identities(self, stanza):
+    def iter_identities(self, stanza=None):
         """
         Return an iterator of tuples describing the identities of the node.
 
         :param stanza: The IQ request stanza
-        :type stanza: :class:`~aioxmpp.IQ`
+        :type stanza: :class:`~aioxmpp.IQ` or :data:`None`
         :rtype: iterable of (:class:`str`, :class:`str`, :class:`str` or :data:`None`, :class:`str` or :data:`None`) tuples
         :return: :xep:`30` identities of this node
 
-        `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
-        used to hide a node depending on who is asking. If the returned
+        `stanza` can be the :class:`aioxmpp.IQ` stanza of the request. This can
+        be used to hide a node depending on who is asking. If the returned
         iterable is empty, the :class:`~.DiscoServer` returns an
         ``<item-not-found/>`` error.
+
+        `stanza` may be :data:`None` if the identities are queried without
+        a specific request context. In that case, implementors should assume
+        that the result is visible to everybody.
+
+        .. note::
+
+           Subclasses must allow :data:`None` for `stanza` and default it to
+           :data:`None`.
 
         Return an iterator which yields tuples consisting of the category, the
         type, the language code and the name of each identity declared in this
@@ -117,7 +126,7 @@ class Node(object):
             if not names:
                 yield category, type_, None, None
 
-    def iter_features(self, stanza):
+    def iter_features(self, stanza=None):
         """
         Return an iterator which yields the features of the node.
 
@@ -129,6 +138,15 @@ class Node(object):
         `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
         used to filter the list according to who is asking (not recommended).
 
+        `stanza` may be :data:`None` if the features are queried without
+        a specific request context. In that case, implementors should assume
+        that the result is visible to everybody.
+
+        .. note::
+
+           Subclasses must allow :data:`None` for `stanza` and default it to
+           :data:`None`.
+
         The features are returned as strings. The features demanded by
         :xep:`30` are always returned.
 
@@ -138,7 +156,7 @@ class Node(object):
             iter(self._features)
         )
 
-    def iter_items(self, stanza):
+    def iter_items(self, stanza=None):
         """
         Return an iterator which yields the items of the node.
 
@@ -150,6 +168,15 @@ class Node(object):
         `stanza` is the :class:`aioxmpp.IQ` stanza of the request. This can be
         used to localize the list to the language of the stanza or filter it
         according to who is asking.
+
+        `stanza` may be :data:`None` if the items are queried without
+        a specific request context. In that case, implementors should assume
+        that the result is visible to everybody.
+
+        .. note::
+
+           Subclasses must allow :data:`None` for `stanza` and default it to
+           :data:`None`.
 
         A bare :class:`Node` cannot hold any items and will thus return an
         iterator which does not yield any element.
@@ -244,7 +271,7 @@ class StaticNode(Node):
         super().__init__()
         self.items = []
 
-    def iter_items(self, stanza):
+    def iter_items(self, stanza=None):
         return iter(self.items)
 
 

tests/disco/test_service.py
@@ -75,6 +75,23 @@ class TestNode(unittest.TestCase):
 
         cb.assert_called_with()
 
+    def test_iter_features_works_without_argument(self):
+        n = disco_service.Node()
+        cb = unittest.mock.Mock()
+        n.on_info_changed.connect(cb)
+
+        n.register_feature("uri:foo")
+
+        self.assertSetEqual(
+            {
+                "uri:foo",
+                namespaces.xep0030_info
+            },
+            set(n.iter_features())
+        )
+
+        cb.assert_called_with()
+
     def test_register_feature_prohibits_duplicate_registration(self):
         n = disco_service.Node()
         cb = unittest.mock.Mock()
@@ -195,6 +212,25 @@ class TestNode(unittest.TestCase):
                 ("client", "pc", None, None),
             },
             set(n.iter_identities(unittest.mock.sentinel.stanza))
+        )
+
+        cb.assert_called_with()
+
+    def test_iter_identities_works_without_stanza(self):
+        n = disco_service.Node()
+
+        cb = unittest.mock.Mock()
+        n.on_info_changed.connect(cb)
+
+        n.register_identity(
+            "client", "pc"
+        )
+
+        self.assertSetEqual(
+            {
+                ("client", "pc", None, None),
+            },
+            set(n.iter_identities())
         )
 
         cb.assert_called_with()
@@ -339,6 +375,13 @@ class TestNode(unittest.TestCase):
             set(n.iter_identities(unittest.mock.sentinel.stanza))
         )
 
+    def test_iter_items_works_without_argument(self):
+        n = disco_service.Node()
+        self.assertSequenceEqual(
+            list(n.iter_items()),
+            []
+        )
+
 
 class TestStaticNode(unittest.TestCase):
     def setUp(self):
@@ -359,6 +402,12 @@ class TestStaticNode(unittest.TestCase):
                 item2
             ],
             list(self.n.iter_items(unittest.mock.sentinel.jid))
+        )
+
+    def test_iter_items_works_without_argument(self):
+        self.assertSequenceEqual(
+            list(self.n.iter_items()),
+            []
         )
 
 