1b71df7d448873107373b7c4a82364d9cb6b55a0
Jonas Wielicki <j.wielicki@sotecware.net>
2017-04-04 21:15:24 +0200

    Use different config for each prosody version

--- utils/install-prosody.sh
+++ utils/install-prosody.sh
@@ -6,7 +6,8 @@ luarocks install luafilesystem
 luarocks install luasocket
 luarocks install luasec
 hg clone https://hg.prosody.im/$PROSODY_BRANCH/ prosody
+curl https://sotecware.net/files/noindex/prosody-cfg-$PROSODY_BRANCH.tar.gz | tar -xz
 cd prosody
 ./configure "--with-lua=$(pwd)/../lua_install"
 make
-
+./patch.sh
