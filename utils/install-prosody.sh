528643c453f3496f8453c356e5b81488f527707b
Jonas Wielicki <j.wielicki@sotecware.net>
2017-04-06 10:24:54 +0200

    Increase verbosity of tar

--- utils/install-prosody.sh
+++ utils/install-prosody.sh
@@ -7,7 +7,7 @@ luarocks install luafilesystem
 luarocks install luasocket
 luarocks install luasec
 hg clone https://hg.prosody.im/$PROSODY_BRANCH/ prosody
-curl https://sotecware.net/files/noindex/prosody-cfg-$PROSODY_BRANCH.tar.gz | tar -xz
+curl https://sotecware.net/files/noindex/prosody-cfg-$PROSODY_BRANCH.tar.gz | tar -vxz
 cd prosody
 ./configure "--with-lua=$(pwd)/../lua_install"
 make
