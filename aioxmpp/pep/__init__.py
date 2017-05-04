fb2221d14166751b14227e6a8d06782b90817ea5
Sebastian Riese <s.riese@zombofant.net>
2017-05-04 15:56:13 +0200

    Improve documentation and hook it into the docs

--- aioxmpp/pep/__init__.py
+++ aioxmpp/pep/__init__.py
@@ -29,8 +29,19 @@ Protocol <163>` subset of :xep:`Publish-Subscribe <60>` comfortably.
 This protocol does not define any XSOs since PEP reuses the protocol
 defined by PubSub.
 
+.. note:: Splitting PEP services into client and server parts is not
+          well supported, since only one claim per PEP node can be made.
+
+.. note:: Payload classes which are to be used with PEP *must* be
+          registered with :func:`aioxmpp.pubsub.xso.as_payload_class`.
+
 .. autoclass:: PEPClient
 
 .. autoclass:: register_pep_node
+
+.. module:: aioxmpp.pep.service
+.. autoclass:: RegisteredPEPNode()
+.. currentmodule:: aioxmpp.pep
+
 """
 from .service import (PEPClient, register_pep_node)  # NOQA
