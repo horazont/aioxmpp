horazont/aioxmpp

commit 07d3effbc4eebe3dc817d485f21b1713ad546a88

Fix exception due to pyasn1 (mis-)usage
Fixes #131.

aioxmpp/security_layer.py
@@ -169,6 +169,9 @@ def extract_python_dict_from_x509(x509):
             if dNSName is None:
                 continue
 
+            if not dNSName.isValue:
+                continue
+
             result.setdefault("subjectAltName", []).append(
                 ("DNS", str(dNSName))
             )

commit 93eb8f468cc73c39c9b52a599bfbd5295e0d6dd9

Fix pyasn1 regression introduced by 07d3eff

aioxmpp/security_layer.py
@@ -169,7 +169,7 @@ def extract_python_dict_from_x509(x509):
             if dNSName is None:
                 continue
 
-            if not dNSName.isValue:
+            if hasattr(dNSName, "isValue") and not dNSName.isValue:
                 continue
 
             result.setdefault("subjectAltName", []).append(