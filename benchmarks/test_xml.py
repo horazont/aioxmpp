31a438f5870f25494ac8904935479aeae3f9ab0d
Jonas Wielicki <j.wielicki@sotecware.net>
2018-05-19 15:22:28 +0200

    xml: add benchmark for xmlValidateNameValue

--- benchmarks/test_xml.py
+++ benchmarks/test_xml.py
@@ -19,7 +19,9 @@
 # <http://www.gnu.org/licenses/>.
 #
 ########################################################################
+import base64
 import io
+import itertools
 import unittest
 import random
 
@@ -87,6 +89,59 @@ class DeepRoot(xso.XSO):
         self.children[:] = [DeepNode() for i in range(3)]
         for child in self.children:
             child.generate(rng, 1)
+
+
+class TestxmlValidateNameValue_str(unittest.TestCase):
+    KEY = "aioxmpp.xml", "xmlValidateNameValue"
+
+    def test_exhaustive(self):
+        validate = aioxmpp.xml.xmlValidateNameValue_str
+
+        r1 = range(0, 0xd800)
+        r2 = range(0xe000, 0xf0000)
+
+        range_iter = itertools.chain(
+            # exclude surrogates
+            r1, r2,
+        )
+
+        with timed() as timer:
+            for cp in range_iter:
+                validate(chr(cp))
+
+        record(self.KEY + ("exhaustive",),
+               timer.elapsed / (len(r1) + len(r2)),
+               "s")
+
+    def test_exhaustive_dualchar(self):
+        validate = aioxmpp.xml.xmlValidateNameValue_str
+
+        strs = ["x" + chr(cp) for cp in range(0, 0xd800)]
+
+        with timed() as timer:
+            for s in strs:
+                validate(s)
+
+        record(self.KEY + ("exhaustive_dualchar",),
+               timer.elapsed / (len(strs)),
+               "s")
+
+    def test_random_strings(self):
+        key = self.KEY + ("random",)
+
+        validate = aioxmpp.xml.xmlValidateNameValue_str
+
+        rng = random.Random(1)
+        samples = []
+        for i in range(1000):
+            samples.append(base64.b64encode(
+                random.getrandbits(120).to_bytes(120//8, 'little')
+            ).decode("ascii").rstrip("="))
+
+        for sample in samples:
+            with timed() as timer:
+                validate(sample)
+            record(key, timer.elapsed, "s")
 
 
 class Testwrite_single_xso(unittest.TestCase):
