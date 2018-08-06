065c938adfe0eb1cc887b23300886d15ca92c59e
Jonas Wielicki <j.wielicki@sotecware.net>
2018-08-06 17:07:25 +0200

    travis: update prosody config for trunk

--- utils/prosody-cfg/trunk/prosody.cfg.lua
+++ utils/prosody-cfg/trunk/prosody.cfg.lua
@@ -68,7 +68,7 @@ modules_enabled = {
 		--"watchregistrations"; -- Alert admins of registrations
 		--"motd"; -- Send a message to users when they log in
 		--"legacyauth"; -- Legacy authentication. Only used by some old clients and bots.
-		"pep_plus";
+		"pep";
 }
 
 -- These modules are auto-loaded, but should you want
