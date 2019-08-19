c0dda4ed49c885f1686f14275353f6fecb4ad897
Jonas Schäfer <j.wielicki@sotecware.net>
2019-08-19 18:24:55 +0200

    Bump version number to 0.10.5

--- aioxmpp/_version.py
+++ aioxmpp/_version.py
@@ -20,7 +20,7 @@
 #
 ########################################################################
 
-version_info = (0, 10, 4, None)
+version_info = (0, 10, 5, None)
 
 __version__ = ".".join(map(str, version_info[:3])) + ("-"+version_info[3] if
                                                       version_info[3] else "")
